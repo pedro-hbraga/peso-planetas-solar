e1114cb617eb11106d1a5570cdff64760be53d16
Pedro Braga <braga-pedro@hotmail.com>
2021-06-19 11:59:07 -0300

    codigo prova

---
+++ PesoPlaneta/src/pesoplaneta/PesoPlaneta.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pesoplaneta;
+
+import view.Janela;
+
+/**
+ *
+ * @author braga
+ */
+public class PesoPlaneta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Janela janela = new Janela();
+        
+        janela.setVisible(true);
+        
+    }
+    
+}
